695044e356aaf51f53b5701b4ad1be3228990f39
MarilynWaldman <mdwaldman22@hotmail.com>
2017-02-22 13:55:03 -0700

    second push

--- HTML/One-Pages/Lawyer/assets/js/plugins/gmaps-ini-rtl.js
+++ HTML/One-Pages/Lawyer/assets/js/plugins/gmaps-ini-rtl.js
@@ -32,7 +32,7 @@ function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 12,
 		scrollwheel: false,
-		center: {lat: 40.674, lng: -73.946},  // Brooklyn.
+		center: {lat: 40.0150, lng: -105.2705},  // Brooklyn.
 		mapTypeControlOptions: {
 			mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
 		}
@@ -45,7 +45,7 @@ function initMap() {
 		map: map,
 		clickable: false,
 		icon: image,
-		position: {lat: 40.674, lng: -73.946}
+		position: {lat: 40.0150, lng: -105.2705}
 	});
 
 	map.mapTypes.set(customMapTypeId, customMapType);
--- HTML/One-Pages/Lawyer/assets/js/plugins/gmaps-ini.js
+++ HTML/One-Pages/Lawyer/assets/js/plugins/gmaps-ini.js
@@ -32,20 +32,20 @@ function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 12,
 		scrollwheel: false,
-		center: {lat: 40.674, lng: -73.946},  // Brooklyn.
+		center: {lat: 40.015, lng: -105.2705},  // Brooklyn.
 		mapTypeControlOptions: {
 			mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
 		}
 	});
 
 	var infowindow = new google.maps.InfoWindow;
-	infowindow.setContent('<b>Brooklyn</b>');
+	infowindow.setContent('<b>Boulder, Colorado</b>');
 
 	var marker = new google.maps.Marker({
 		map: map,
 		clickable: false,
 		icon: image,
-		position: {lat: 40.674, lng: -73.946}
+		position: {lat: 40.015, lng: -105.2705}
 	});
 
 	map.mapTypes.set(customMapTypeId, customMapType);
